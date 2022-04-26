fa7248ef6aa21d3edde14ac4a1fdbd7f014fe66a
shahirulhafizsapri <shahirulhafizsapri@gmail.com>
2022-04-26 11:12:42 +0800

    pola

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ const {
 
 const main = async () => {
   const pathPackage = core.getInput('package-dictionary');
+  const propertiesName = core.getInput('properties-name');
+  let propertyList = propertiesName.split(',');
   // git log --follow -p -- package.json
+  
   const command = "git show"
      const {
       stdout,
       stderr
    } = await exec(command);
-  console.log("stdout v1", stdout)
+  console.log("stdout v1", stdout.trim().split('\r\n'))
+
+
 
   // console.log("Current Directory", __dirname)
   let content = await fs.readFile(pathPackage, 'utf8')
